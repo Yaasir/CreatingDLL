e464a2402edba94090a8ec405c839c6a20476f2b
Yaasir <m.yaasir@live.com>
2014-05-12 08:28:15 +0400

    initial commit

---
+++ CreatingDLL/Class1.cs
@@ -0,0 +1,30 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Class1.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace CreatingDLL
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class Class1
+    {
+        /// <summary>
+        /// sum is method that take two integer value and return that sum
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public int sum(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
